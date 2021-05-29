b7eaa0668bc59d189e7a614acc5d2d9790f97038
vaaaarlos <kadu.cerq@gmail.com>
2021-05-29 19:18:25 -0300

    Initial project structure

---
+++ pitchy/src/main/java/com/lojfacens/pitchy/PitchyApplication.java
@@ -0,0 +1,13 @@
+package com.lojfacens.pitchy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PitchyApplication.class, args);
+  }
+
+}
---
+++ pitchy/src/test/java/com/lojfacens/pitchy/PitchyApplicationTest.java
@@ -0,0 +1,12 @@
+package com.lojfacens.pitchy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchyApplicationTest {
+
+  @Test
+  void contextLoads() { }
+
+}
