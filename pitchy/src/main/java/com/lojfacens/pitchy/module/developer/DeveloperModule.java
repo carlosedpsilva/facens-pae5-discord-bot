bb2cfe5746a89dcf61faef396095f424e75e4701
vaaaarlos <kadu.cerq@gmail.com>
2021-06-10 14:32:33 -0300

    feat: :sparkles: Add command argument splitting

---
+++ pitchy/src/main/java/com/lojfacens/pitchy/module/developer/DeveloperModule.java
@@ -0,0 +1,11 @@
+package com.lojfacens.pitchy.module.developer;
+
+import com.lojfacens.pitchy.service.command.meta.CommandModule;
+
+public class DeveloperModule extends CommandModule {
+
+  public DeveloperModule() {
+    super("developer");
+  }
+
+}
--- pitchy/src/main/java/com/lojfacens/pitchy/service/command/meta/CommandContext.java
+++ pitchy/src/main/java/com/lojfacens/pitchy/service/command/meta/CommandContext.java
@@ -1,6 +1,10 @@
 package com.lojfacens.pitchy.service.command.meta;
 
+import java.util.List;
+import java.util.Map;
+
 import com.lojfacens.pitchy.service.main.BotManager;
+import com.lojfacens.pitchy.util.StringUtils;
 
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +12,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.SelfUser;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
@@ -40,5 +45,31 @@ public class CommandContext {
   public SelfUser getSelfUser() { return getJDA().getSelfUser(); }
 
   public Member getSelfMember() { return getGuild().getSelfMember(); }
+
+  public boolean hasArguments() { return content != null; }
+
+  public String[] getArguments() { return getArguments(-1); }
+
+  public String[] getArguments(int expectedArgs) { return StringUtils.advancedSplitArgs(content, expectedArgs); }
+
+  public Map<String, List<String>> getOptionalArguments() { return StringUtils.parseArguments(getArguments()); }
+
+  public void send(Message message) { getChannel().sendMessage(message); }
+
+  public void send(String message) { getChannel().sendMessage(message); }
+
+  public void send(MessageEmbed embed) { getChannel().sendMessage(embed).queue(s -> {}, Throwable::printStackTrace); }
+
+  public void reply(Message message) { getMessage().reply(message).queue(s -> {}, f -> send(message)); }
+
+  public void reply(String message) { getMessage().reply(message).queue(s -> {}, f -> send(message)); }
+
+  public void reply(MessageEmbed embed) { getMessage().reply(embed).queue(s -> {}, f -> send(embed)); }
+
+  public void sendFormat(String message, Object... format) {
+    getChannel().sendMessage(
+        String.format(message, format)
+    ).queue();
+  }
 
 }
